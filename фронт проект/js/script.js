8f18f673e74b8e858c17b980c19d15be943cbb2b
axmed dadashzade <55343836+ahmad6272@users.noreply.github.com>
2020-01-06 23:12:02 +0400

    Add files via upload

--- фронт проект/js/script.js
+++ фронт проект/js/script.js
@@ -1,50 +1,81 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // home
     $(".home_menu").hide();
-    $(".home").click(function(){
+    $(".home").click(function () {
         $(".home_menu").removeClass("d-none");
         $(".home_menu").toggle(1000);
     })
-    
+
     // page
     $(".pages_menu").hide();
-    $(".page").click(function(){
+    $(".page").click(function () {
         $(".page_menu").removeClass("d-none");
         $(".page_menu").toggle(1000);
     })
-    
+
     // shop
 
     // portfolio
     $(".portfolio_menu").hide();
-    $(".portfolio").click(function(){
+    $(".portfolio").click(function () {
         $(".portfolio_menu").removeClass("d-none");
         $(".portfolio_menu").toggle(1000)
     })
 
     // blog
     $(".blog_menu").hide();
-    $(".blog").click(function(){
+    $(".blog").click(function () {
         $(".blog_menu").removeClass("d-none");
         $(".blog_menu").toggle(1000)
     })
 
     // elements
     $(".elements_menu").hide();
-    $(".elm").click(function(){
+    $(".elm").click(function () {
         $(".elements_menu").removeClass("d-none");
         $(".elements_menu").toggle(1000)
     })
 
 
     // slider
-    
+
     // filter in shelf
     $(".filter_menu").hide();
-    $(".filter").click(function(){
+    $(".filter").click(function () {
         $(".filter_menu").removeClass("d-none");
         $(".filter_menu").toggle(1000)
     })
-          
+
+
+    // accardion
+    $(".first").click(function () {
+        $(".first_body").not($(this).next()).slideUp(500);
+        $(this).next().slideToggle(500);
+    });
+
+    $(".second").click(function () {
+        $(".second_body").not($(this).next()).slideUp(500);
+        $(this).next().slideToggle(500);
+    });
+
+    $(".third").click(function () {
+        $(".third_body").not($(this).next()).slideUp(500);
+        $(this).next().slideToggle(500);
+    });
+
+    $(".fourth").click(function () {
+        $(".fourth_body").not($(this).next()).slideUp(500);
+        $(this).next().slideToggle(500);
+    });
+
+    $(".card-header").click(function () {
+        $(this).addClass("color");
+    })
+
+
+    // tab
+    $(".tab-item").click(function () {
+        $
+    })
 
 }); 
