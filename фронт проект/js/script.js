887e3dca681a8db4e1187ee0ba45b7b4468e03fd
axmed dadashzade <55343836+ahmad6272@users.noreply.github.com>
2020-01-08 00:26:54 +0400

    Add files via upload

--- фронт проект/js/script.js
+++ фронт проект/js/script.js
@@ -1,43 +1,4 @@
 $(document).ready(function () {
-    // home
-    $(".home_menu").hide();
-    $(".home").click(function () {
-        $(".home_menu").removeClass("d-none");
-        $(".home_menu").toggle(1000);
-    })
-
-    // page
-    $(".pages_menu").hide();
-    $(".page").click(function () {
-        $(".page_menu").removeClass("d-none");
-        $(".page_menu").toggle(1000);
-    })
-
-    // shop
-
-    // portfolio
-    $(".portfolio_menu").hide();
-    $(".portfolio").click(function () {
-        $(".portfolio_menu").removeClass("d-none");
-        $(".portfolio_menu").toggle(1000)
-    })
-
-    // blog
-    $(".blog_menu").hide();
-    $(".blog").click(function () {
-        $(".blog_menu").removeClass("d-none");
-        $(".blog_menu").toggle(1000)
-    })
-
-    // elements
-    $(".elements_menu").hide();
-    $(".elm").click(function () {
-        $(".elements_menu").removeClass("d-none");
-        $(".elements_menu").toggle(1000)
-    })
-
-
-    // slider
 
     // filter in shelf
     $(".filter_menu").hide();
@@ -74,8 +35,13 @@ $(document).ready(function () {
 
 
     // tab
-    $(".tab-item").click(function () {
-        $
-    })
+    $("#tab_item a").click(function(){
+		$(".tab div").hide(); 
+		$(".tab div").filter(this.hash).show(); 
+		$("#tab_item a").removeClass("show");
+		$(this).addClass("show");
+	})
+
+
 
 }); 
